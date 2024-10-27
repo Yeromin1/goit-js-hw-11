12e59bef6682c4c15f0087b0fe4ab00c288df143
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2024-10-27 13:43:22 +0200

    oshibka

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { fetchImages } from './js/pixabay-api.js';
 import { renderImages } from './js/render-functions.js';
-import iziToast from 'izitoast';
-import 'iziToast/dist/css/iziToast.min.css';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import iziToast from 'izitoast';
+// import 'iziToast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
+// const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
 form.addEventListener('submit', async event => {
@@ -15,17 +14,17 @@ form.addEventListener('submit', async event => {
   const query = queryInput.value.trim();
   queryInput.value = '';
 
-  if (!query) {
-    iziToast.warning({
-      title: 'Warning',
-      message: 'Please enter a search term.',
-      position: 'topRight',
-    });
-    return;
-  }
+  // if (!query) {
+  //   iziToast.warning({
+  //     title: 'Warning',
+  //     message: 'Please enter a search term.',
+  //     position: 'topRight',
+  //   });
+  //   return;
+  // }
 
-  loader.classList.remove('hidden');
-  gallery.innerHTML = '';
+  // loader.classList.remove('hidden');
+  // gallery.innerHTML = '';
 
   fetchImages(query)
     .then(images => {
